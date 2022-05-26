6695119a7169d133faa4635e8c4edb4d32067e58
vladfilipro <vlad.filip.devl@gmail.com>
2022-05-26 16:58:12 +0300

    Packages update and loader export

--- scripts/utils/loader.js
+++ scripts/utils/loader.js
@@ -33,10 +33,11 @@ class Loader {
    * Preload an image
    * @param {String} id A unique identifier for this file
    * @param {String} url The image url to load
+   * @param {Image} image The image oject used to load the asset. If null a new one will be created.
    * @return {Promise} Returns a promise which is resolved upon loading
    */
-  preloadImage ( id, url ) {
-    let img = new Image()
+  preloadImage ( id, url, image ) {
+    let img = image || new Image()
     return new Promise( ( resolve, reject ) => {
       img.crossOrigin = 'anonymous'
       img.onload = () => {
@@ -54,20 +55,21 @@ class Loader {
    * Preload an audio file
    * @param {String} id A unique identifier for this file
    * @param {String} url The audio url to load
+   * @param {Audio} audio The audio oject used to load the asset. If null a new one will be created.
    * @return {Promise} Returns a promise which is resolved upon loading
    */
-  preloadAudio ( id, url ) {
-    let audio = new Audio()
+  preloadAudio ( id, url, audio ) {
+    let sound = audio || new Audio()
     return new Promise( ( resolve, reject ) => {
-      audio.crossOrigin = 'anonymous'
-      audio.addEventListener( 'canplay', () => {
-        this.__cache[ id ] = audio
+      sound.crossOrigin = 'anonymous'
+      sound.addEventListener( 'canplay', () => {
+        this.__cache[ id ] = sound
         resolve( this.__cache[ id ] )
       } )
-      audio.onerror = ( err ) => {
+      sound.onerror = ( err ) => {
         reject( err )
       }
-      audio.src = url
+      sound.src = url
     } )
   }
 
@@ -75,10 +77,11 @@ class Loader {
    * Preload a file using HTTPRequest
    * @param {String} id A unique identifier for this file
    * @param {String} url The file url to load
+   * @param {XMLHttpRequest} request The xhr oject used to load the asset. If null a new one will be created.
    * @return {Promise} Returns a promise which is resolved upon loading
    */
-  preloadFile ( id, url ) {
-    let xhr = new XMLHttpRequest()
+  preloadFile ( id, url, request ) {
+    let xhr = request || new XMLHttpRequest()
     return new Promise( ( resolve, reject ) => {
       xhr.open( 'GET', url )
       xhr.withCredentials = true
@@ -95,7 +98,7 @@ class Loader {
 
   /**
    * Load a list of files
-   * @param {Object} files Contains a list of files to load
+   * @param {Object} files Contains a list of files to load with the following structure: { "file1": "url1", "file2": "url2" } or { "file1": { type: "image|audio|file", url: "url1", obj: objectLoader|null } }
    * @param {Function} progress A callback function with the parameters: "err, id, data, loaded, total"
    * @return {Promise} Returns a promise which is resolved upon loading all files, doesn't matter if they failed or not
    */
@@ -121,13 +124,15 @@ class Loader {
 
       loop( files, ( file, id ) => {
         let type = 'file'
+        let obj = null
         let url = file
         if ( isObject( file ) ) {
           type = file.type
           url = file.url
+          obj = file.obj || null
         }
         let loader = this[ 'preload' + type.charAt( 0 ).toUpperCase() + type.slice( 1 ).toLowerCase() ]
-        loader.call( this, id, url )
+        loader.call( this, id, url, obj )
           .then( ( data ) => {
             loaded++
             process( null, id, data, loaded, total )
